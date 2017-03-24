8b656528b691517706ac067e425f3d1183c04434
Madhurjya Pegu <migom6@gmail.com>
2017-03-24 16:53:04 +0530

    added recommend endpoint

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var tone = require('./app/tone').tone;
+var recommend = require('./app/recommend').recommend;
 
 app.listen(3000, function (req, res) {
     console.log("server started");
@@ -21,3 +22,11 @@ app.get('/text/:id', function (req, res) {
     tone(req.params.id, myCallback);
 });
 
+app.get('/recommend/:id', function (req, res) {
+
+    console.log(req.params.id);
+    function myCallback(retvalue) {
+        console.log("inside callback" + retvalue);
+    };
+    recommend(req.params.id, myCallback);
+});

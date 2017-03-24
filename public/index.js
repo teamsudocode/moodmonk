8f5e03bbe4b6bde1c08cda89477ca885c4ada9a9
Abhishek Sharma <hello.abhishek@outlook.com>
2017-03-24 20:50:39 +0530

    added words

--- public/index.js
+++ public/index.js
@@ -16,41 +16,54 @@ var close = function () {
     document.body.style.backgroundImage = "background-image: radial-gradient(circle, #00b5ff 0%, #26b2fa 70%, #1fd2fb 100%)";
     try {
         if (listening) stopListening();
-    } catch (e) {}
+    } catch (e) { }
+    var html = document.getElementsByTagName('html')[0];
+    html.style.setProperty("--bgcolor", '#26b2fa');
 };
 
 var change = function (mood) {
     "use strict";
     console.log('changing to ' + mood);
-    let moodcolor,textcolor;
+    let moodcolor, textcolor, moodtext;
     switch (mood) {
-    case ("anger"):
-        moodcolor = "#e53935";
-        textcolor = "white";
-        break;
-    case ("disgust"):
-        moodcolor = "#2e7d32";
-        textcolor = "white";
-        break;
-    case ("fear"):
-        moodcolor = "#5e35b1";
-        textcolor = "white";
-        break;
-    case ("joy"):
-        moodcolor = "#ffeb3b";
-        textcolor = "black";
-        break;
-    case ("sadness"):
-        moodcolor = "#0d47a1";
-        textcolor = "white";
-        break;
+        case ("anger"):
+            moodcolor = "#e53935";
+            textcolor = "white";
+            moodtext = "Anger";
+            break;
+        case ("disgust"):
+            moodcolor = "#2e7d32";
+            textcolor = "white";
+            moodtext = "Disgust";
+            break;
+        case ("fear"):
+            moodcolor = "#5e35b1";
+            textcolor = "white";
+            moodtext = "Fear";
+            break;
+        case ("joy"):
+            moodcolor = "#ffeb3b";
+            textcolor = "white";
+            moodtext = "Joy";
+            break;
+        case ("sadness"):
+            moodcolor = "#0d47a1";
+            textcolor = "white";
+            moodtext = "Sadness";
+            break;
     }
     var html = document.getElementsByTagName('html')[0];
     html.style.setProperty("--bgcolor", moodcolor);
-    document.getElementById('word').innerHTML = mood;
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.id = 'word';
+    document.getElementById('word').textContent = moodtext;
     document.getElementById('word').style.color = textcolor;
     document.getElementById('word').className = "animated zoomIn";
-    document.getElementById('word').className = "animated zoomOut";
+    document.getElementById('word').style.opacity = 0;
+    setTimeout(function(){
+        $( "#word" ).remove();
+    },2000);
 };
 
 document.getElementById("start").addEventListener('click', begin);

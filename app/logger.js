d08cff5975929b3339b05703fe2d289fd0720c33
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-03-24 20:45:58 +0530

    create getDataByDate

--- app/logger.js
+++ app/logger.js
@@ -10,9 +10,7 @@ function getDateKey() {
     let dd = today.getDate();
     let mm = today.getMonth()+1; //January is 0!
     let yyyy = today.getFullYear();
-    if(dd<10) dd='0'+dd;
-    if(mm<10) mm='0'+mm;
-    return dd+'/'+mm+'/'+yyyy;
+    return mm+'-'+dd+'-'+yyyy;
 }
 
 function log(json) {
@@ -31,7 +29,7 @@ function log(json) {
 
 function incrementDay(key, json) {
     /* assumed that this function is invoked only when
-       an object for same day exist
+     an object for same day exist
      */
     let toneCategories = json.document_tone.tone_categories;
     // console.log(JSON.stringify(data));
@@ -60,6 +58,36 @@ function dominantEmotion(json){
     return dominant_emotion;
 }
 
+function getDataByDate(date) {
+    console.log("finding by date" + date);
+    if (!data.hasOwnProperty(date)) {
+        return "404";
+    }
+    let json = data[date];
+    let retvalue = {
+        "emotion_tone": {
+            "anger": json.document_tone.tone_categories[0].tones[0].score,
+            "disgust": json.document_tone.tone_categories[0].tones[1].score,
+            "fear": json.document_tone.tone_categories[0].tones[2].score,
+            "sadness": json.document_tone.tone_categories[0].tones[3].score,
+            "joy": json.document_tone.tone_categories[0].tones[4].score
+        },
+        "language_tone": {
+            "analytical": json.document_tone.tone_categories[1].tones[0].score,
+            "confident": json.document_tone.tone_categories[1].tones[1].score,
+            "tentative": json.document_tone.tone_categories[1].tones[2].score
+        },
+        "social_tone": {
+            "openness": json.document_tone.tone_categories[2].tones[0].score,
+            "conscientiousness": json.document_tone.tone_categories[2].tones[1].score,
+            "extraversion": json.document_tone.tone_categories[2].tones[2].score,
+            "agreableness": json.document_tone.tone_categories[2].tones[3].score,
+            "emotional_range": json.document_tone.tone_categories[2].tones[4].score
+        }
+    };
+    return retvalue;
+}
+
 function analyzer(data){
     let emotions = ['anger', 'disgust', 'fear', 'sadness', 'joy'];
     for (let i in data){
@@ -69,7 +97,8 @@ function analyzer(data){
 var logger = {
     'analyzer': analyzer,
     'dominantEmotion': dominantEmotion,
-    'log': log
+    'log': log,
+    'getByDate': getDataByDate
 };
 
 module.exports = { logger };

92da96572529fac71f01feada31ef8cf985dda40
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-03-24 15:46:59 +0530

    completed logger

--- app/logger.js
+++ app/logger.js
@@ -1,35 +1,57 @@
-var data = require("../data/data.json")
+// const anondataFile = '../data/anonymous.json';
+// const dataFileName = '../data/data.json';
+const dataFileName = "/home/himanshu/Dropbox/development/github/moodmonk/data/data.json";
+
+var data = require(dataFileName);
 var fs = require("fs");
 
-function logger(json){
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-
-    var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd;
-    }
-    if(mm<10){
-        mm='0'+mm;
-    }
-    today = dd+'/'+mm+'/'+yyyy;
-    if (!data.hasOwnProperty(today)){
-        data[today] = json;
-    }
-    else {
-        increment_day(today, json);
-    }
-    fs.writeFile("../data/data.json", JSON.stringify(data), "utf8");
+function getDateKey() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //January is 0!
+    let yyyy = today.getFullYear();
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return dd+'/'+mm+'/'+yyyy;
 }
 
-function incrementDay(today, json){
+function log(json) {
+    console.log('data' + JSON.stringify(data));
+    let key = getDateKey();
+    if (!data.hasOwnProperty(key)) {
+        data[key] = json;
+        console.log(data);
+        fs.writeFile(dataFileName, JSON.stringify(data), "utf8", () => {
+            console.log("data updated");
+        });
+    } else {
+        incrementDay(key, json);
+    }
 }
+
+function incrementDay(key, json) {
+    /* assumed that this function is invoked only when
+       an object for same day exist
+     */
+    let toneCategories = json.document_tone.tone_categories;
+    // console.log(JSON.stringify(data));
+    for (let i = 0; i < toneCategories.length; i++) {
+        for (let j = 0; j < toneCategories[i].tones.length; j++) {
+            data[key].document_tone.tone_categories[i].tones[j].score +=
+                json.document_tone.tone_categories[i].tones[j].score;
+        }
+    }
+    // console.log(JSON.stringify(data));
+    fs.writeFile(dataFileName, JSON.stringify(data), "utf8", () => {
+        console.log('data updated by incrementing');
+    });
+}
+
 function dominantEmotion(json){
-    var max = 0.0;
-    var dominant_emotion = '';
-    var emotions =json["document_tone"]["tone_categories"][0]["tones"]; 
-    for (var emotion in emotions){
+    let max = 0.0;
+    let dominant_emotion = '';
+    let emotions =json["document_tone"]["tone_categories"][0]["tones"];
+    for (let emotion in emotions){
         if (emotions[emotion].score > max){
             max = emotions[emotion].score;
             dominant_emotion = emotions[emotion].tone_id;
@@ -39,7 +61,15 @@ function dominantEmotion(json){
 }
 
 function analyzer(data){
-    var emotions = ['anger', 'disgust', 'fear', 'sadness', 'joy']
-    for (var i in data){
+    let emotions = ['anger', 'disgust', 'fear', 'sadness', 'joy'];
+    for (let i in data){
     }
 }
+
+var logger = {
+    'analyzer': analyzer,
+    'dominantEmotion': dominantEmotion,
+    'log': log
+};
+
+module.exports = { logger };

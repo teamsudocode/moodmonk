10eefd2d461e804e18bb0f657e3048de8ff848dc
Priyanshu Kumar <priyanshukm@gmail.com>
2017-04-19 15:45:25 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var script = require(__dirname+'/app/mysqlquery.js');
 
-script.getEmotionTone(10,1, function(obj) {
-    console.log("Done\n");
+var ob = {userid : 1, date : 2, anger : 3, disgust : 4 , fear : 5,joy : 6,sadness : 7};
+script.setEmotionTone(ob, function(obj) {
+    console.log(obj);
 });

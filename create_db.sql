a885ce7eaee9b21b493230ca789729f86d5793e2
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-04-19 16:03:35 +0530

    add create_db script

---
+++ create_db.sql
@@ -0,0 +1,45 @@
+use moodmonk;
+
+drop table if exists User;
+create table User (
+`userid` varchar(255) not null,
+`password` varchar(255) not null,
+primary key (`userid`)
+);
+
+drop table if exists emotion_tone;
+create table emotion_tone (
+`index`   int auto_increment,
+`userid`  varchar(255) not null,
+`date`    date not null,
+`anger`   decimal(11, 10) not null,
+`disgust` decimal(11, 10) not null,
+`fear`    decimal(11, 10) not null,
+`joy`     decimal(11, 10) not null,
+`sadness` decimal(11, 10) not null,
+primary key (`index`, `userid`, `date`)
+);
+
+drop table if exists language_tone;
+create table language_tone (
+`index`       int auto_increment,
+`userid`      varchar(255) not null,
+`date`        date not null,
+`analytical`  decimal(11, 10) not null,
+`confident`   decimal(11, 10) not null,
+`tentative`   decimal(11, 10) not null,
+primary key (`index`, `userid`, `date`)
+);
+
+drop table if exists social_tone;
+create table social_tone (
+`index`                  int auto_increment,
+`userid`                 varchar(255) not null,
+`date`                   date not null,
+`openness_big5`          decimal(11, 10) not null,
+`conscientiousness_big5` decimal(11, 10) not null,
+`extraversion_big5`      decimal(11, 10) not null,
+`aggreableness_big5`     decimal(11, 10) not null,
+`emotional_range_big5`   decimal(11, 10) not null,
+primary key (`index`, `userid`, `date`)
+);
